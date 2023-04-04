468e559109e9f1928e9d8b8a8eb9c2b0d0c17807
Lorenzo <uni.lorenzo.a@gmail.com>
2023-04-04 17:44:12 +0200

    Update

--- main.py
+++ main.py
@@ -1,73 +1,71 @@
-from eth_account import Account
 from web3 import Web3, HTTPProvider
 #from numba import jit, cuda
 from timeit import default_timer as timer
-import re
 
 import multiprocessing
+import threading
+import wallet
 
-pattern = '0x666' #0x324e2d42d7b65e5574787c331dfaa29d2dead666 sostituisci "123" con la stringa desiderata per l'indirizzo vanity
-
-infura_endpoint='https://mainnet.infura.io/v3/972b4b8a084b434bba97948637e946be'
-alchemy_endpoint='https://eth-mainnet.g.alchemy.com/v2/csmoa4n5DwucntIE0vZtb5NtOiJLC_ez'
-
-connection = Web3(HTTPProvider(alchemy_endpoint))
-
-
-def generateAccount():
-    acct = Account.create()
-    address = acct.address#.lower()
-
-    private_key = acct.key.hex()
-
-    return private_key, address
-
-def checkBalanceETH(address):
-    #safe_address = connection.to_checksum_address(address)
-    #print("Safe: " + str(safe_address))
-    wei_balance = connection.eth.get_balance(address)
-    balance = connection.from_wei(wei_balance, 'ether')
-
-    return balance
-
-def generateVanity(pattern, private_key, public_key):
-    if re.match(pattern, public_key):
-        print(f"Vanity address found: {public_key}")
-        print(f"Private key: {private_key}")
-
-        return private_key, public_key
-
-    return None
+pattern = '0x324e2d42d7b65e5574787c331dfaa29d2dead666'
 
 #@jit(target_backend='cuda')
-def randomGenerateCUDA():
-    while True:
-        private_key, public_key = generateAccount()
-        balance = checkBalanceETH(public_key)
+def random_generate(connection):
+    private_key, public_key = wallet.generateAccount()
+    balance = wallet.checkBalanceETH(connection, public_key)
 
-        print(public_key)
+    return private_key, public_key, balance
+def check_infura_address():
+    print('Start random generate with Infura')
+    connection = Web3(HTTPProvider(wallet.infura_endpoint))
+
+    address_with_balance_list = []
+    for i in range(0, wallet.INFURA_LIMIT-1):
+        private_key, public_key, balance = random_generate(connection)
+
         if balance > 0.0:
-            print("Private key: " + str(private_key), " Balance: " + str(balance))
+            data = {'private_key': private_key, 'public_key': public_key, 'balance': balance}
+            address_with_balance_list.append(data)
 
-def randomGenerate():
-    while True:
-        private_key, public_key = generateAccount()
-        balance = checkBalanceETH(public_key)
+    return data
 
-        #print(public_key)
+
+def check_alchemy_address():
+    print('Start random generate with Alchemy')
+    connection = Web3(HTTPProvider(wallet.alchemy_endpoint))
+
+    address_with_balance_list = []
+    for i in range(0, wallet.ALCHEMY_LIMIT - 1):
+        private_key, public_key, balance = random_generate(connection)
+
         if balance > 0.0:
-            print("Private key: " + str(private_key), " Balance: " + str(balance))
+            data = {'private_key': private_key, 'public_key': public_key, 'balance': balance}
+            address_with_balance_list.append(data)
+
+    return data
+
+def generate_vanity_eth(pattern):
+    print('Start Vanity ETH: ', pattern)
+
+    private_key, public_key = wallet.generateAccount()
+    check = wallet.check_vanity(pattern, private_key, public_key)
+
+    while not check:
+        private_key, public_key = wallet.generateAccount()
+        check = wallet.check_vanity(pattern, private_key, public_key)
+
+    print('Found Private_Key: ', private_key, ' Public_Key: ', public_key)
+
+
+
 
 
 
 if __name__=="__main__":
-    NUM_PROC = 8
 
     jobs = []
-
-    for i in range(NUM_PROC):
-        process = multiprocessing.Process(target=randomGenerate)
-        jobs.append(process)
+    jobs.append(multiprocessing.Process(target=check_infura_address))
+    jobs.append(multiprocessing.Process(target=check_alchemy_address))
+    jobs.append(multiprocessing.Process(target=generate_vanity_eth, args=pattern))
 
     for j in jobs:
         j.start()
--- wallet.py
+++ wallet.py
@@ -6,6 +6,9 @@ import re
 infura_endpoint = 'https://mainnet.infura.io/v3/972b4b8a084b434bba97948637e946be'
 alchemy_endpoint = 'https://eth-mainnet.g.alchemy.com/v2/csmoa4n5DwucntIE0vZtb5NtOiJLC_ez'
 
+INFURA_LIMIT = 100000
+ALCHEMY_LIMIT = 300000000
+
 
 def generateAccount():
     acct = Account.create()
@@ -29,18 +32,18 @@ def generatePublicKeyFromPrivate(private_key):
 
     return address
 
-def checkBalanceETH(connection, address):
+def checkBalanceETH(connection, address, coin='ether'):
     #safe_address = connection.to_checksum_address(address)
     #print("Safe: " + str(safe_address))
     wei_balance = connection.eth.get_balance(address)
-    balance = connection.from_wei(wei_balance, 'ether')
+    balance = connection.from_wei(wei_balance, coin)
 
     return balance
 
-def generateVanity(pattern, private_key, public_key):
+def check_vanity(pattern, private_key, public_key):
     if re.match(pattern, public_key):
-        print(f"Vanity address found: {public_key}")
-        print(f"Private key: {private_key}")
+        #print(f"Vanity address found: {public_key}")
+        #print(f"Private key: {private_key}")
 
         return private_key, public_key
 
