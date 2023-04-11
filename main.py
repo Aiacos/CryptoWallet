609c095b1552e908a969c633811cb1087ef664e4
Lorenzo <uni.lorenzo.a@gmail.com>
2023-04-11 12:20:52 +0200

    Refactor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from web3 import Web3, HTTPProvider
-#from numba import jit, cuda, njit
+# from numba import jit, cuda, njit
 from timeit import default_timer as timer
 
 import multiprocessing
@@ -22,92 +22,103 @@ def send_email(subject, body):
         subject=subject,
         contents=body
     )
-#@jit(target_backend='cuda')
-def random_generate(connection):
-    private_key, public_key = wallet.generateAccount()
 
-    try:
-        balance = wallet.checkBalanceETH(connection, public_key)
-    except:
-        balance = 0.0
 
-    return private_key, public_key, balance
+class RandomGenerateAddress:
+    def __init__(self, connection):
+        self.connection = connection
+        self.valid_address_list = []
+        self.vanity_address_list = []
+
+    def generate_account(self, mode='mnemonic'):
+        private_key, public_key, mnemonic = wallet.generateAccount(mode)
+
+        return private_key, public_key, mnemonic
+
+    def get_balance(self, address):
+        balance = wallet.checkBalanceETH(self.connection, address)
+
+        return balance
+
+    def generate_and_check_balance(self, mode):
+        private_key, public_key, mnemonic = self.generate_account(mode)
+        balance = self.get_balance(public_key)
+
+        return {'private_key': private_key, 'public_key': public_key, 'balance': balance, 'mnemonic: ': mnemonic}
+
+    def check_vanity(self, vanity_address, public_key, regex=True):
+        return wallet.check_vanity(vanity_address, public_key, regex)
+
+    def generate_address(self, iteration_limit, gen_mode='mnemonic', vanity_address=None, regex=True, check_balance=True, console_print=True, console_print_empty=False):
+        if iteration_limit == 0:
+            while True:
+                key = self.generate_and_check_balance(gen_mode)
+                if console_print_empty: print(key)
+
+                if vanity_address:
+                    if self.check_vanity(vanity_address, key['public_key'], regex):
+                        self.vanity_address_list.append(key)
+                        if console_print: print(key)
+
+                if check_balance and key['balance'] > 0.0:
+                    self.valid_address_list.append(key)
+                    if console_print: print(key)
+        else:
+            for i in range(0, iteration_limit):
+                key = self.generate_and_check_balance(gen_mode)
+                if console_print_empty: print(key)
+
+                if vanity_address:
+                    if self.check_vanity(vanity_address, key['public_key'], regex):
+                        self.vanity_address_list.append(key)
+                        if console_print: print(key)
+
+                if check_balance and key['balance'] > 0.0:
+                    self.valid_address_list.append(key)
+                    if console_print: print(key)
+
 def check_infura_address(limit):
     connection = Web3(HTTPProvider(wallet.infura_endpoint))
 
-    address_with_balance_list = []
-    for i in range(0, limit):
-        private_key, public_key, balance = random_generate(connection)
+    infura_instance = RandomGenerateAddress(connection)
+    infura_instance.generate_address(limit)
 
-        if balance > 0.0:
-            keys = {'private_key': private_key, 'public_key': public_key, 'balance': balance}
-            address_with_balance_list.append(keys)
-
-    print('Infura generator done')
-
-    data = []
-    for line in address_with_balance_list:
-        line_string = str(line)
-        data.append(line_string)
-
-    if data:
-        try:
-            send_email('Infura Report', "\n".join(data))
-        except:
-            print("\n".join(data))
-
-    return address_with_balance_list
-
-def check_alchemy_address(limit):
+def check_alchemy_address(limit=0):
     connection = Web3(HTTPProvider(wallet.alchemy_endpoint))
 
-    address_with_balance_list = []
-    for i in range(0, limit):
-        private_key, public_key, balance = random_generate(connection)
+    infura_instance = RandomGenerateAddress(connection)
+    infura_instance.generate_address(limit)
 
-        if balance > 0.0:
-            keys = {'private_key': private_key, 'public_key': public_key, 'balance': balance}
-            address_with_balance_list.append(keys)
 
-    print('Alchemy generator done')
 
-    data = []
-    for line in address_with_balance_list:
-        line_string = str(line)
-        data.append(line_string)
-
-    if data:
-        try:
-            send_email('Infura Report', "\n".join(data))
-        except:
-            print("\n".join(data))
-
-    return address_with_balance_list
 
 def process_generate_infura(n_threads=8):
     schedule.every().day.at("00:01").do(generate_random_address_with_balance_infura, n_threads)
 
     while True:
         try:
-            schedule.run_pending()
-            #schedule.run_all()
+            # schedule.run_pending()
+            schedule.run_all()
         except:
             print('Pass Infura')
 
         time.sleep(60)  # wait one minute
 
+
 def process_generate_alchemy(n_threads=8):
-    schedule.every().day.at("00:01").do(generate_random_address_with_balance_alchemy, n_threads)
+    schedule.every().day.at("10:13").do(generate_random_address_with_balance_alchemy, n_threads)
 
     while True:
         try:
-            if date.today().day == 1:
+            if date.today().day == 11:
                 schedule.run_pending()
-                #schedule.run_all()
+                # schedule.run_all()
         except:
             print('Pass Alchemy')
 
         time.sleep(60)  # wait one minute
+
+
 def generate_random_address_with_balance_infura(n_threads=8):
     print('Start random generate with Infura')
 
@@ -115,35 +126,37 @@ def generate_random_address_with_balance_infura(n_threads=8):
 
     thread_list = []
     for t in range(0, n_threads):
-        infura_gen = threading.Thread(target=check_infura_address, args=(limit, ))
+        infura_gen = threading.Thread(target=check_infura_address, args=(limit,))
         thread_list.append(infura_gen)
 
     for t in thread_list:
         t.start()
 
+
 def generate_random_address_with_balance_alchemy(n_threads=8):
     print('Start random generate with Alchemy')
 
-    limit = int(wallet.INFURA_LIMIT / n_threads)
+    limit = int(wallet.ALCHEMY_LIMIT / n_threads)
 
     thread_list = []
     for t in range(0, n_threads):
-        alchemy_gen = threading.Thread(target=check_alchemy_address, args=(limit, ))
+        alchemy_gen = threading.Thread(target=check_alchemy_address, args=(0,))
         thread_list.append(alchemy_gen)
 
     for t in thread_list:
         t.start()
+
 
 def generate_vanity_eth(pattern):
     private_key, public_key = wallet.generateAccount()
     check = wallet.check_vanity(pattern, private_key, public_key)
 
     while not check:
-        #start = timer()
+        # start = timer()
         private_key, public_key = wallet.generateAccount()
         check = wallet.check_vanity(pattern, private_key, public_key)
-        #print("Time:", timer() - start)
-        #print(private_key, public_key)
+        # print("Time:", timer() - start)
+        # print(private_key, public_key)
 
     try:
         send_email('Infura Report', str('Found Private_Key: ', private_key, ' Public_Key: ', public_key))
@@ -153,27 +166,35 @@ def generate_vanity_eth(pattern):
     return check
 
 
-
 def process_generate_vanity_eth(pattern, n_threads=12):
     print('Start Vanity ETH: ', pattern)
 
     thread_list = []
     for t in range(0, n_threads):
-        vanity_gen = threading.Thread(target=generate_vanity_eth, args=(pattern, ))
+        vanity_gen = threading.Thread(target=generate_vanity_eth, args=(pattern,))
         thread_list.append(vanity_gen)
 
     for t in thread_list:
         t.start()
 
 
-
-if __name__=="__main__":
-    #yagmail.register('username', 'password')
+if __name__ == "__main__":
+    # yagmail.register('username', 'password')
 
     jobs = []
-    jobs.append(multiprocessing.Process(target=process_generate_infura, args=(8, ), name='Infura_ETH'))
-    jobs.append(multiprocessing.Process(target=process_generate_alchemy, args=(8, ), name='Alchemy_ETH'))
-    jobs.append(multiprocessing.Process(target=process_generate_vanity_eth, name='Vanity_ETH', args=(pattern.lower(), pow(2, 8)-1)))
+    # jobs.append(multiprocessing.Process(target=process_generate_infura, args=(8, ), name='Infura_ETH'))
+    # jobs.append(multiprocessing.Process(target=process_generate_alchemy, args=(8, ), name='Alchemy_ETH'))
+    # jobs.append(multiprocessing.Process(target=process_generate_vanity_eth, name='Vanity_ETH', args=(pattern.lower(), pow(2, 8)-1)))
 
     for j in jobs:
         j.start()
+
+    connection = Web3(HTTPProvider(wallet.alchemy_endpoint))
+    print(connection.isConnected())
+    while True:
+        private_key, public_key, balance, mnemonic = random_generate(connection)
+
+        # print(mnemonic, balance)
+        if balance and balance > 0.0:
+            keys = {'private_key': private_key, 'public_key': public_key, 'balance': balance, 'Mnemonic: ': mnemonic}
+            print(keys)
