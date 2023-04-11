a69966430f49d068cb9b4876d41eb9ce5539d51f
Lorenzo <uni.lorenzo.a@gmail.com>
2023-04-11 11:20:44 +0200

    Update

--- wallet.py
+++ wallet.py
@@ -15,21 +15,31 @@ ALCHEMY_LIMIT = 300000000000
 
 
 
-def generateAccount(fast=True):
-    if fast:
+def generateAccount(mode='mnemonic'):
+    if mode == 'fast':
         p_key = keccak_256(token_bytes(32)).digest()
         public_key = PublicKey.from_valid_secret(p_key).format(compressed=False)[1:]
         addr = keccak_256(public_key).digest()[-20:]
 
         private_key = p_key.hex()
         address = '0x' + addr.hex()
+
+    elif mode == 'mnemonic':
+        Account.enable_unaudited_hdwallet_features()
+        acct, mnemonic = Account.create_with_mnemonic()
+        address = acct.address#.lower()
+
+        private_key = acct.key.hex()
+
+        return private_key, address, mnemonic
+
     else:
         acct = Account.create()
         address = acct.address#.lower()
 
         private_key = acct.key.hex()
 
-    return private_key, address
+    return private_key, address, None
 
 def generatePrivateKey():
     acct = Account.create()
@@ -49,7 +59,7 @@ def checkBalanceETH(connection, address, coin='ether'):
     #safe_address = connection.to_checksum_address(address)
     #print("Safe: " + str(safe_address))
     wei_balance = connection.eth.get_balance(address)
-    balance = connection.from_wei(wei_balance, coin)
+    balance = connection.fromWei(wei_balance, coin)
 
     return balance
 
