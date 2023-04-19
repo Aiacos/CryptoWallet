7a37ee53a7943cbeb432591933a430834d7addba
Lorenzo <uni.lorenzo.a@gmail.com>
2023-04-19 17:08:39 +0200

    Update

--- BTC_Scanner.py
+++ BTC_Scanner.py
@@ -1,5 +1,7 @@
 import sys
 import itertools
+#from itertools import batched
+import more_itertools
 from bit import Key
 from tqdm import tqdm
 import threading
@@ -7,8 +9,8 @@ import threading
 pattern = '13zb1hQbWVsc2S7ZTZnP2G4undNNpdh5so'
 range_str = '20000000000000000:3ffffffffffffffff'
 
-pattern = '1GnNTmTVLZiqQfLbAdp9DVdicEnB5GoERE'
-range_str = '20000:3ffff'
+pattern = '1LeBZP5QCwwgXRtmVUvTVrraqPUokyLHqe'
+range_str = '200:3ff'
 
 
 def generate_account(hex=None, scalar=None):
@@ -28,53 +30,51 @@ def convert_split(range_str, divisions=100):
 
     int_min = int(range_min, 16)
     int_max = int(range_max, 16)
-    chunk = (int_max - int_min) / (divisions - 1)
+    chunk = int((int_max - int_min) / (divisions))
 
     it = range(int_min, int_max)
-    l = itertools.tee(it, divisions)
+    l = chunks_generator(it, chunk)
     data = {'min': int_min, 'max': int_max, 'iterator_len': sum(1 for _ in l), 'chunk': chunk, 'range_iterator': l}
 
     return data
 
-def sub_iterator(iterator):
-    return iterator.start(), iterator.stop()
+
+def sub_iterator(it):
+    int_min = more_itertools.first(it)
+    int_max = more_itertools.last(it)
+
+    print(int_min, int_max)
+    print(it)
+    for c in tqdm(it, desc='Sedondary Loop'):
+        print(c)
+        priv, address = generate_account(scalar=c)
+        #print(priv, address)
+        if address == pattern:
+            result = priv, address
+            print('Result: ', result)
+
+            return result
 
 
-################### OLD ###################
-def divide_chunks(l, n):
-    # looping till length l
-    for i in range(0, len(l), n):
-        yield l[i:i + n - 1]
+def chunks_generator(iterable, size):
+    """Generate adjacent chunks of data"""
 
-def generate_batch(min, max, p=100):
-    r = range(min, max)
-    chunk = (max - min) / p
-    splitted_list = divide_chunks(r, int(chunk))
+    it_slice = more_itertools.divide(size, iterable)
 
-    return splitted_list, chunk
+    return it_slice
+
 
 def main_loop(pattern, range_str):
     dt = convert_split(range_str, 100)
-    #print(dt)
+    print(dt)
     for i in tqdm(dt['range_iterator'], desc='Main Loop'):
-        #print(sub_iterator(i))
-        for c in tqdm(i, desc='Sedondary Loop'):
-            priv, address = generate_account(scalar=c)
-            #print(priv, address)
-            if address == pattern:
-                result = priv, address
-                print('Result: ', result)
-                return result
-                break
-        else:
-            continue
-        break
+        print('Result: ', sub_iterator(i))
+
 
 if __name__ == "__main__":
     argv = sys.argv
     main_loop(argv[1], argv[2])
-
-
+    #main_loop(pattern, range_str)
 
     """
     n_threads = 10
@@ -92,4 +92,3 @@ if __name__ == "__main__":
     for t in thread_list:
         t.start()
     """
-
